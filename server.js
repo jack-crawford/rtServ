07d13413cd6e81a76c833202a8a8291d96bb4adc
Jack Crawford <jack@Jacks-MacBook-Pro-5.local>
2020-10-26 17:32:28 -0400

    make it better

--- server.js
+++ server.js
@@ -26,9 +26,4 @@ wss.broadcast = function(data) {
   });
 };
 
-console.log('Server running. Visit https://localhost:' + (8080) + ' in Firefox/Chrome.\n\n\
-Some important notes:\n\
-  * Note the HTTPS; there is no HTTP -> HTTPS redirect.\n\
-  * You\'ll also need to accept the invalid TLS certificate.\n\
-  * Some browsers or OSs may not allow the webcam to be used by multiple pages at once. You may need to use two different browsers or machines.\n'
-);
+console.log('Server running. Visit https://localhost:' + (process.env.PORT || 3000) + ' in Firefox/Chrome.\n\n');
